02f2aa5a73fa5a8618c1f55b7eadb1f00429914a
Rajkiran reddy <Rajkiran reddy@192.168.0.6>
2019-09-24 16:51:16 +0530

    succesfull

--- DEL/src/DELRECRD.java
+++ DEL/src/DELRECRD.java
@@ -1,16 +1,12 @@
 import java.sql.*;
-public class UPDATEMULTPLE {
+public class DELRECRD {
 public static void main(String[] args) {
 	Connection conn=null;
 	try {
 		Class.forName("oracle.jdbc.driver.OracleDriver");   
 				conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","RAJKIRAN","Raj@1996");  //connect to database//
 			Statement st = conn.createStatement(); 
-			st.executeUpdate("UPDATE officials  SET dept='hr' WHERE username='gopal'");
-			st.executeUpdate("UPDATE admin      SET email='srikant@gmail.com' WHERE username='srikant'");
-			st.executeUpdate("UPDATE users      SET username='tirupati23' WHERE username='tiru23'");
-			st.executeUpdate("UPDATE users      SET email='vijay@gmail.com' WHERE name='vijay'");
-
+			st.executeUpdate("DELETE FROM users WHERE name='tirupati'");
 	
 			conn.close();
 	}
