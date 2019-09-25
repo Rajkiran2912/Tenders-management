b941424d8f10437e4e81ad0cec1884c37223a5fc
Rajkiran reddy <Rajkiran reddy@192.168.0.4>
2019-09-25 14:17:40 +0530

    view successfull

--- VIEW/src/VIEW1.java
+++ VIEW/src/VIEW1.java
@@ -1,16 +1,15 @@
+
 import java.sql.*;
-public class DELMULT {
+
+public class VIEW1 {
+
 public static void main(String[] args) {
 	Connection conn=null;
 	try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");   //load driver//
+		Class.forName("oracle.jdbc.driver.OracleDriver");   
 				conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","RAJKIRAN","Raj@1996");  //connect to database//
 			Statement st = conn.createStatement(); 
-			st.executeUpdate("DELETE FROM users WHERE username='raju2912'");
-			st.executeUpdate("DELETE FROM admin WHERE username='alex'");
-			st.executeUpdate("DELETE FROM users WHERE email='sam@xyz.com'");
-			st.executeUpdate("DELETE FROM officials WHERE dept='hr'");
-	
+			st.executeUpdate("CREATE VIEW officialsdesig AS SELECT username FROM officials WHERE design='exe eng' ");
 			conn.close();
 	}
 	catch (Exception e)
